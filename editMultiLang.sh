ada19b7c0c1ac105d39d70f5d1d8964775a5ccb5
value should not be mesured in $ but in resuced lifes <mail@marclandolt.ch>
2016-12-19 05:07:24 +0100

    edit Multilang

--- editMultiLang.sh
+++ editMultiLang.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-#unfortunately inkscape -p has a bug in debian jessie => Workarround
-
-echo "
-
-
-"
-
-echo -n "install required debian jessie packages (y/n)? "
-read answer
-if echo "$answer" | grep -iq "^y" ;then
-sudo apt-get install xdotool figlet
-else
-    echo not installing packages
-fi
-
 echo "
 
 "
 
 find ./cards -name "*.svg" >./print.txt
-
-
-
- /usr/bin/inkscape &
-
 
 
  for i in $(cat ./print.txt)

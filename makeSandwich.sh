933e7174c73511e5d9cea22bf5d86cc8f2d8d00c
value should not be mesured in $ but in resuced lifes <mail@marclandolt.ch>
2017-01-13 11:20:41 +0100

    cleaned

--- makeSandwich.sh
+++ makeSandwich.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ./scripts/install.sh
-./scripts/generatePNGs.sh
-./scripts/generateA4.sh
-./scripts/generateA6.sh
+./scripts/generatePNGs.sh DE
+./scripts/generateA4.sh DE
+./scripts/generateA6.sh DE
+./scripts/generatePNGs.sh EN
+./scripts/generateA4.sh EN
+./scripts/generateA6.sh EN
 #./scripts/printPDF.sh
 ./scripts/printAll.sh DE
 ./scripts/printAll.sh EN
